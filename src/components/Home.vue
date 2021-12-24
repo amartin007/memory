89dc9135be4c741cafd074fe033d8043478b0004
Anthony Martin <amartin007@gmail.com>
2021-12-23 22:37:48 -0600

    Added pug templating

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,21 @@
-<template>
-  <!-- LAYOUT -->
-  <div class="bg-white drop-shadow-lg flex items-center justify-center p-3">
-    <h1 class="text-3xl font-bold">MEMORY</h1>
-  </div>
-  <!-- SCOREBOARD -->
-  <!-- MAIN CONTAINER -->
-  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
-    <div class="grid grid-cols-3 md:grid-cols-4 gap-6">
-      <div
-        v-for="index in 16"
-        :key="index"
-        class="bg-white p-9 rounded border border-black"
-      >
-        {{ index }}
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+//- LAYOUT
+.bg-white.drop-shadow-lg.p-3.flex-center
+  h1.text-3xl.font-bold MEMORY
+//- SCOREBOARD
+//- MAIN CONTAINER
+.bg-gray-100.min-h-screen.pt-9.flex-center
+  .grid.grid-cols-3.md_grid-cols-4.gap-6
+    div.bg-white.p-9.rounded.border.border-black(
+      v-for="index in 16"
+      :key="index"
+    ) {{ index }}
 </template>
 <script>
 export default {};
 </script>
-<style lang=""></style>
+<style lang="css">
+.flex-center {
+  @apply flex items-center justify-center;
+}
+</style>
