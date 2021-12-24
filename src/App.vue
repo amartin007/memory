2defb5d591a8ba1b9f4c737c15d7a975b7bba04d
Anthony Martin <amartin007@gmail.com>
2021-12-23 22:15:04 -0600

    Added home page and grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-center">
-    Memory
-  </h1>
+  <Home />
 </template>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- LAYOUT -->
+  <div class="bg-white drop-shadow-lg flex items-center justify-center p-3">
+    <h1 class="text-3xl font-bold">MEMORY</h1>
+  </div>
+  <!-- SCOREBOARD -->
+  <!-- MAIN CONTAINER -->
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="grid grid-cols-3 md:grid-cols-4 gap-6">
+      <div
+        v-for="index in 16"
+        :key="index"
+        class="bg-white p-9 rounded border border-black"
+      >
+        {{ index }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
