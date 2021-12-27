d2a12c870db8c754388b5e2d48bce603d0c7997d
Anthony Martin <amartin007@gmail.com>
2021-12-27 17:57:14 -0600

    Updated styling and responsive grid

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,22 @@
 <template lang="pug">
 //- LAYOUT
-.bg-white.drop-shadow-lg.p-3.flex-center
-  h1.text-3xl.font-bold MEMORY
+.bg-sky-800.shadow-lg.shadow-slate-700.p-3.flex-center.relative
+  h1.text-3xl.font-bold.text-white MEMORY
 //- SCOREBOARD
 //- MAIN CONTAINER
-.bg-gray-100.min-h-screen.pt-9.flex-center
-  .grid.grid-cols-3.md_grid-cols-4.gap-6
-    div.bg-white.p-9.rounded.border.border-black(
-      v-for="index in 16"
-      :key="index"
-    ) {{ index }}
+.bg-white.py-9
+  //- CARD
+  .mx-auto.p-6.bg-stone-700.rounded-lg.shadow-lg.shadow-slate-700(class="w-1/2")
+    .grid.grid-cols-2.md_grid-cols-3.lg_grid-cols-4.gap-6
+      .bg-black.aspect-square.p-9.rounded-lg.text-white.flex-center(
+        v-for="index in 16"
+        :key="index"
+      ) {{ index }}
 </template>
 <script>
 export default {};
 </script>
-<style lang="css">
+<style>
 .flex-center {
   @apply flex items-center justify-center;
 }
