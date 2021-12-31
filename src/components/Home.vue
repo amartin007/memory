6b2fc62e12cb0155c8b89bbc321643072857c88f
Anthony Martin <amartin007@gmail.com>
2021-12-31 13:01:52 -0600

    Added images/icons, and deck shuffle/setup on load

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,93 @@
 <template lang="pug">
 //- LAYOUT
 .bg-sky-800.shadow-lg.shadow-slate-700.p-3.flex-center.relative
-  h1.text-3xl.font-bold.text-white MEMORY
+  h1.text-3xl.font-bold.text-white TILE MATCH
 //- SCOREBOARD
 //- MAIN CONTAINER
 .bg-white.py-9
+  .text-center
+    label.p-3 Category:
+    select.mb-3.border.border-black.px-6.py-2.rounded-md(
+      v-model="selectedCategory"
+      @change="setupDeck"
+    )
+      option(value="animals" text="Animals")
+      option(value="nfcTeams" text="NFC Teams")
   //- CARD
   .mx-auto.p-6.bg-stone-700.rounded-lg.shadow-lg.shadow-slate-700(class="w-1/2")
     .grid.grid-cols-2.md_grid-cols-3.lg_grid-cols-4.gap-6
-      .bg-black.aspect-square.p-9.rounded-lg.text-white.flex-center(
+      .bg-black.aspect-square.p-4.rounded-lg.text-white.flex-center(
         v-for="index in 16"
         :key="index"
-      ) {{ index }}
+      ) 
+        font-awesome-icon(v-if="selectedCategory=='animals'" size="4x" :icon="['fas', matchArray[index - 1]]")
+        img(v-else-if="selectedCategory=='nfcTeams'" :src="`/src/images/${matchArray[index-1]}.webp`")
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selectedCategory: 'animals',
+      animals: [
+        'cat',
+        'crow',
+        'dog',
+        'dove',
+        'dragon',
+        'feather',
+        'feather-alt',
+        'fish',
+        'frog',
+        'hippo',
+        'horse',
+        'horse-head',
+        'kiwi-bird',
+        'otter',
+        'paw',
+        'spider'
+      ],
+      nfcTeams: [
+        '49ers',
+        'bears',
+        'buccaneers',
+        'cardinals',
+        'cowboys',
+        'eagles',
+        'falcons',
+        'football-team',
+        'giants',
+        'lions',
+        'packers',
+        'panthers',
+        'rams',
+        'saints',
+        'seahawks',
+        'vikings'
+      ],
+      matchArray: []
+    };
+  },
+  methods: {
+    shuffle(deck) {
+      deck.sort(() => 0.5 - Math.random());
+    },
+    setupDeck() {
+      //copy the array
+      const deck = [...this[this.selectedCategory]];
+      //shuffle the array
+      this.shuffle(deck);
+      //get first 8 from the deck
+      const cardsToSelect = deck.splice(0, 8);
+      //add them twice to the matchArray
+      this.matchArray = [...cardsToSelect, ...cardsToSelect];
+      //shuffle the deck a final time
+      this.shuffle(this.matchArray);
+    }
+  },
+  beforeMount() {
+    this.setupDeck();
+  }
+};
 </script>
 <style>
 .flex-center {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,47 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/tailwind.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './styles/tailwind.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCat,
+  faCrow,
+  faDog,
+  faDove,
+  faDragon,
+  faFeather,
+  faFeatherAlt,
+  faFish,
+  faFrog,
+  faHippo,
+  faHorse,
+  faHorseHead,
+  faKiwiBird,
+  faOtter,
+  faPaw,
+  faSpider
+} from '@fortawesome/free-solid-svg-icons';
+// import {  } from '@fortawesome/free-brands-svg-icons';
+// import {  } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).mount('#app')
+library.add([
+  faCat,
+  faCrow,
+  faDog,
+  faDove,
+  faDragon,
+  faFeatherAlt,
+  faFeather,
+  faFeatherAlt,
+  faFish,
+  faFrog,
+  faHippo,
+  faHorse,
+  faHorseHead,
+  faKiwiBird,
+  faOtter,
+  faPaw,
+  faSpider
+]);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
