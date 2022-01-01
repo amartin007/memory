8b574544568aec17cc84e12d99992c1d90ea0de3
Anthony Martin <amartin007@gmail.com>
2022-01-01 11:43:16 -0600

    Moved images to pub dir to work in prod

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,7 +26,7 @@
             :class="{ 'bg-red-400': card.mismatched, 'bg-blue-400': card.matched, 'bg-green-400': card.selected && !card.matched && !card.mismatched }"
           )
             font-awesome-icon(v-if="selectedCategory=='animals'" size="4x" :icon="['fas', card.name]")
-            img(v-else-if="selectedCategory=='nfcTeams'" :src="`/src/images/${card.name}.webp`" :class="{ 'hidden': !card.selected }")
+            img(v-else-if="selectedCategory=='nfcTeams'" :src="`/images/${card.name}.webp`" :class="{ 'hidden': !card.selected }")
         
 </template>
 
